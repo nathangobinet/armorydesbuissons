6d475410db4f1a6da31571026ab751a52b5ab2eb
Nathan Gobinet <nathan.gobinet@gmail.com>
2020-04-01 00:51:02 +0200

    Add AOS and animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 import 'bootstrap';
+import Aos from 'aos';
 import './nav/toggleNav';
 import './nav/toggleNavSelects';
+
+Aos.init({
+  offset: 120,
+  delay: 100,
+  duration: 600,
+  disable: 'mobile',
+});
