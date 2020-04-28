430ee2a9b254dbf8c80c6d33892e03e6e703f56f
Nathan Gobinet <nathan.gobinet@gmail.com>
2020-04-28 23:06:29 +0200

    Add 2 sections - Add graph

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
 import Aos from 'aos';
+import Chart from 'chart.js';
 import './nav/toggleNav';
 import './nav/toggleNavSelects';
 
@@ -10,6 +11,70 @@ Aos.init({
   disable: 'mobile',
 });
 
-function style({style}) {
 
-}
+const lineChartData = {
+  labels: ['02/04', '03/04', '04/04', '05/04', '06/04', '07/04', '08/04'],
+  datasets: [{
+    label: 'Kills',
+    borderColor: '#63BF60',
+    backgroundColor: '#63BF60',
+    fill: false,
+    data: [
+      10,
+      5,
+      56,
+      12,
+      54,
+      10,
+      10,
+    ],
+    yAxisID: 'y-axis-1',
+  }, {
+    label: 'Players',
+    borderColor: '#F26E50',
+    backgroundColor: '#F26E50',
+    fill: false,
+    data: [
+      21,
+      12,
+      21,
+      12,
+      21,
+      21,
+      54,
+    ],
+    yAxisID: 'y-axis-2',
+  }],
+};
+
+
+const ctx = document.getElementById('canvas');
+const chart = new Chart(ctx, {
+  type: 'line',
+  data: lineChartData,
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      position: 'bottom',
+    },
+    scales: {
+      yAxes: [{
+        type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+        display: true,
+        position: 'left',
+        id: 'y-axis-1',
+      }, {
+        type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+        display: true,
+        position: 'right',
+        id: 'y-axis-2',
+
+        // grid line settings
+        gridLines: {
+          drawOnChartArea: false, // only want the grid lines for one axis to show up
+        },
+      }],
+    },
+  },
+});
