944deb4acfb8722fecbb9c0b8153660391d07aee
Nathan Gobinet <nathan.gobinet@gmail.com>
2020-09-29 00:46:01 +0200

    Cleanup next config and fix referrer issue on chrome by setting to Referrer-Policy:no-referrer-when-downgrade

--- next.config.js
+++ next.config.js
@@ -1,28 +1,31 @@
 const withOptimizedImages = require('next-optimized-images');
 const withVideos = require('next-videos');
-const compose = require('next-compose');
 
-module.exports = compose([
-  [withOptimizedImages, withVideos({
-    assetDirectory: 'static',
-    webpack(config) {
-      return config;
-    },
-  })],
-  {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.(ogg|mp3|wav|mpe?g)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              name: '[name]-[hash].[ext]',
-            },
-          },
-        ],
-      });
-      return config;
-    },
+module.exports = withOptimizedImages(withVideos({
+  async headers() {
+    return [{
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Referrer-Policy',
+          value: 'no-referrer-when-downgrade',
+        },
+      ],
+    }];
   },
-]);
+  assetDirectory: 'static',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(mp3|wav|mpe?g)$/i,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            name: '[name]-[hash].[ext]',
+          },
+        },
+      ],
+    });
+    return config;
+  },
+}));
